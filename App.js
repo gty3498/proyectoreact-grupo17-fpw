f0b901f48d7c697b50b411c5cc9a8722c7c5a8ed
CaucotaFernando <112452983+gty3498@users.noreply.github.com>
2022-09-14 22:48:20 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import divv from "./div.js";
+
+function App() {
+  return (
+    <divv/>
+  );
+}
+
+export default App;
---
+++ div.js
@@ -0,0 +1,33 @@
+import React from "react";
+class divv extends React.Component{
+    render(){
+        return(
+            <><header>
+
+                <title>Peliculas</title>
+
+             </header><body>
+                    <h1>Nuestras recomendaciones</h1>
+                    <div>
+                        <img src="https://pics.filmaffinity.com/D_jame_salir-290175782-large.jpg" alt="dsdsd" /><p>Chris va a conocer a los padres de Rose, su novia. Al principio se muestran encantadores, si bien un tanto nerviosos. Chris supone que se debe a que él es negro, pero un poco más tarde descubre que sus suegros ocultan un secreto espeluznante.</p>
+                        <h1>IMDb 7,7/10</h1>
+                    </div>
+
+                </body></>
+
+
+
+
+        )
+
+
+
+
+    }
+
+
+
+
+}
+
+export default divv;
